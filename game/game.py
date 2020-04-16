6b5f95ff76c9a80951b5d51bc8dc8baec21d14ad
Misha <melnykgm@gmail.com>
2020-04-17 00:29:46 +0300

    new vers

--- game/game.py
+++ game/game.py
@@ -1,15 +1,24 @@
 import random
+import sys
 
 def main():
-  print(get_info())
-  make_decision(get_user_choice(),get_comp_choice(1,3))
-  
+  score = [0,0]
+  while True:
+    print(get_info())
+    winner = make_decision(get_user_choice(),get_comp_choice(1,3))
+    score=update_score(score, winner)
+    print(winner)
+    print(score)
+    exit_program()
+    
+    
 
 def get_info():
-  info="""Make your choice:
+  info = """Make your choice:
 1 - scissors
 2 - rock
-3 - paper"""
+3 - paper
+4 - exit"""
   return info
 
 def get_user_choice():
@@ -20,23 +29,51 @@ def get_comp_choice(x,y):
   comp_choice = random.randint(x,y)
   return comp_choice
 
-def make_decision(user_choice,comp_choice):
-  if user_choice == comp_choice:
+def exit_program():
+  bye = "Thanks for the game, bye"
+  return bye
+
+def update_score(score,winner):
+  if winner=='user':
+    score[0] =+ 1
+  elif winner=='pc':
+    score[1] =+ 1 
+  return score
+
+
+    
+def make_decision(user_choice, comp_choice):
+  if user_choice==comp_choice:
     print("draw")
-  if user_choice == 1 and comp_choice==2:
-    print("you win")
-  if user_choice == 1 and comp_choice==3:
-    print("You lose")
+    winner='draw'
+    
+  elif user_choice==1 and comp_choice==2 or user_choice==2 and comp_choice==3 or user_choice==3 and comp_choice==1:
+    winner='user'
+    
+    
+  elif user_choice==1 and comp_choice==3 or user_choice==2 and comp_choice==1 or user_choice==3 and comp_choice==2:
+    winner='pc'
 
-  if user_choice==2 and comp_choice==3:
-    print("you win")
-  if user_choice==2 and comp_choice==1:
-    print("You lose")
 
-  if user_choice==3 and comp_choice==2:
-    print("you lose")
-  if user_choice==3 and comp_choice==1:
-    print("You win")
+  elif user_choice==4:
+    print exit_program()
+    sys.exit()
+
+
+
+  return winner
+
+
+    
+
+  
+
+  print winner()
+  update_score(winner_name,score)
+    
+  
+
+ 
 
 
 main()
