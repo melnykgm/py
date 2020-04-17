989202a21d6c60efeebd9d973ad1d0688e1b628c
Misha <melnykgm@gmail.com>
2020-04-17 17:47:34 +0300

    changes

--- game/game.py
+++ game/game.py
@@ -1,79 +1,71 @@
 import random
 import sys
 
+
 def main():
-  score = [0,0]
-  while True:
-    print(get_info())
-    winner = make_decision(get_user_choice(),get_comp_choice(1,3))
-    score=update_score(score, winner)
-    print(winner)
-    print(score)
-    exit_program()
-    
-    
+    score = [0, 0]
+    while True:
+        print(get_info())
+        winner = make_decision(get_user_choice(), get_comp_choice(1, 3))
+        score = update_score(score, winner)
+        print(winner)
+        print(score)
+        exit_program()
+
 
 def get_info():
-  info = """Make your choice:
+    info = """Make your choice:
 1 - scissors
 2 - rock
 3 - paper
 4 - exit"""
-  return info
+    return info
+
 
 def get_user_choice():
-  user_choice = int(raw_input())
-  return user_choice
+    user_choice = int(raw_input())
+    return user_choice
 
-def get_comp_choice(x,y):
-  comp_choice = random.randint(x,y)
-  return comp_choice
+
+def get_comp_choice(x, y):
+    comp_choice = random.randint(x, y)
+    return comp_choice
+
 
 def exit_program():
-  bye = "Thanks for the game, bye"
-  return bye
-
-def update_score(score,winner):
-  if winner=='user':
-    score[0] =+ 1
-  elif winner=='pc':
-    score[1] =+ 1 
-  return score
+    bye = "Thanks for the game, bye"
+    return bye
 
 
-    
+def update_score(score, winner):
+    if winner == 'user':
+        score[0] = + 1
+    elif winner == 'pc':
+        score[1] = + 1
+    return score
+
+
 def make_decision(user_choice, comp_choice):
-  if user_choice==comp_choice:
-    print("draw")
-    winner='draw'
-    
-  elif user_choice==1 and comp_choice==2 or user_choice==2 and comp_choice==3 or user_choice==3 and comp_choice==1:
-    winner='user'
-    
-    
-  elif user_choice==1 and comp_choice==3 or user_choice==2 and comp_choice==1 or user_choice==3 and comp_choice==2:
-    winner='pc'
+    if user_choice == comp_choice:
+        print("draw")
+        winner = 'draw'
+
+    elif user_choice == 1 and comp_choice == 2 or user_choice == 2 and comp_choice == 3 or user_choice == 3 and comp_choice == 1:
+        winner = 'user'
 
 
-  elif user_choice==4:
-    print exit_program()
-    sys.exit()
+    elif user_choice == 1 and comp_choice == 3 or user_choice == 2 and comp_choice == 1 or user_choice == 3 and comp_choice == 2:
+        winner = 'pc'
 
 
+    elif user_choice == 4:
+        print exit_program()
+        sys.exit()
 
-  return winner
+    return winner
 
-
-    
-
-  
-
-  print winner()
-  update_score(winner_name,score)
-    
-  
-
- 
+    print winner()
+    update_score(winner_name, score)
 
 
 main()
